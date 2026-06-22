58f01bc9885a5a99530b714ddc13ad4c5dd4b57d
prdaxca <100952967+FORTY71@users.noreply.github.com>
2026-06-23 02:02:03 +0800

    Create connect.js

---
+++ api/connect.js
@@ -0,0 +1,32 @@
+export default function handler(req, res) {
+  // Mengatur header agar merespons dalam format JSON
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 'no-store, max-age=0, no-cache');
+
+  // Mengembalikan status 200 OK beserta data payload
+  res.status(200).json({
+    "status": true,
+    "data": {
+      "real": "PUBG-Teamxcracking606-2c213b26-fbec-3f9b-99b2-22fe9117b75a-Vm8Lk7Uj2JmsjCPVPVjrLa7zgfx3uz9E",
+      "token": "8f3a8c41e9a0941483f5e7f15b6f19e2",
+      "modname": "Auto Draw V11 Pro VIP",
+      "mod_status": "Safe",
+      "credit": "Safe",
+      "ESP": "off",
+      "Item": "off",
+      "AIM": "off",
+      "SilentAim": "off",
+      "BulletTrack": "off",
+      "Floating": "on",
+      "Memory": "off",
+      "Setting": "on",
+      "expired_date": "2026-06-29 04:22:23",
+      "EXP": "2027-06-29 04:22:23",
+      "ts": "2027-06-29 04:22:23",
+      "exdate": "2026-06-29 04:22:23",
+      "device": "1000",
+      "rng": 1782151112,
+      "realdata": "9+oM2gO0QLqDzaSOsBDiA4NTafcZALnZV9XBoWeAs68="
+    }
+  });
+}
